bd7fd67ea6198673b81a96990a363969d8741fdc
Habib Wahid <habibwahid199183@gmail.com>
2024-10-06 22:04:01 +0600

    JavaScript begginers lesson for font-end developers

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+
+// Array add and remove element from beginners
+var teaLine = ["Habib", "Wahid", "Jhonkar Mahbub", "Anisul Islam"]; //কোন একটা Array নির্ধারন করার জন্য। 
+teaLine.push("Jalal"); // .push() array টি হলো কোন একটি শব্দ পুরাতন array তে সর্বশেষে যোগ করার জন্য। 
+teaLine.pop();  // .pop() array টি হলো পুরাতন array থেকে সর্বশেষ শব্দটি ডিলেট করার জন্য। 
+console.log(teaLine);
+
+// learning while loop
+var num = 0;
+while (num <= 5){
+    console.log(num);
+    num++;
+}
