0923c4bd3afaa99deb090490c1177d9b0cd2fb88
Habib Wahid <habibwahid199183@gmail.com>
2024-10-08 17:23:28 +0600

    Javascript core part from begginers

--- index.js
+++ index.js
@@ -292,9 +292,19 @@ console.log(result);
 
 
 
+console.log("..........javascript prime number check from begginers..............")
 
 
+// ....................Javascript prime number check from begginers.....................
 
-
-
+function isPrime(n){
+    for ( i = 2; i < n; i++){
+        if(n%i == 0){
+            return "not a prime number";
+        }
+    }
+    return "Your number is a prime number";
+}
+var result = isPrime(79);
+console.log(result);
 
